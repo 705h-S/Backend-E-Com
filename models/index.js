641083fcfe2177d1131e154fec3b9629dd1d00d6
705H-S <93358442+705h-S@users.noreply.github.com>
2022-02-09 21:28:26 -0800

    fixed 2nd bug code: 'ER_NO_REFERENCED_ROW_2' errno: 1452... lines 19 & 25 on index.js

--- models/index.js
+++ models/index.js
@@ -16,15 +16,15 @@ Category.hasMany(Product, {
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
-through: ProductTag,
+through: 'ProductTag',
 foreignKey: 'product_id',
 foreignKey: 'tag_id',
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
-  through: ProductTag,
-  foreignKey: 'product_id',
+  through: 'ProductTag',
   foreignKey: 'tag_id',
+  foreignKey: 'product_id',
   });
 
 module.exports = {
